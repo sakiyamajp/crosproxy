f0524389ad5f78bc853578e6afc6692b4b40955e
saky <saky>
2019-08-25 15:09:40 +0000

    add white list

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 8080;
 
 var cors_proxy = require('cors-anywhere');
 cors_proxy.createServer({
-    originWhitelist: [], // Allow all origins
+    originWhitelist: ["https://liquidtrades.s3-ap-northeast-1.amazonaws.com"], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2']
 }).listen(port, host, function() {
